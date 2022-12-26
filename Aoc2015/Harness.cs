5199770ffaff946f88d6c8b44f0f96e91cda8873
Marvin <marvk3@gmail.com>
2022-12-27 00:27:37 +0100

    Initial commit

---
+++ Aoc2015/Harness.cs
@@ -0,0 +1,3 @@
+﻿namespace Aoc2015 {
+
+}
---
+++ Aoc2015/Program.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace Aoc2015 {
+    internal static class Program {
+        private static void Main(string[] args) {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
